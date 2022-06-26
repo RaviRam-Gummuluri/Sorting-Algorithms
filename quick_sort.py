f45f46327b114bee3a114f4760dae28a067f5ed0
GV Ravi Ram <gummulurivenkataraviram.211ai018@nitk.edu.in>
2022-06-26 11:15:32 +0530

    edited for roshan

--- quick_sort.py
+++ quick_sort.py
@@ -1,7 +1,10 @@
+def quick_sort:
+    
+
 n=int(input("enter number of elements to sort : "))
 a=[]
 for x in range(0,n):
     a.append(int(input("enter the number : ")))
 s=int(input("enter 0 for sorting in ascending and 1 for sorting in descending : "))
-sel_sort(a,n,s)
+quick_sort(a,n,s)
 print(a)
